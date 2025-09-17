ce088120d06007f7c64ae3b515cee24531379f5d
Khanh Nguyen <khanhtn@tamu.edu>
2025-09-17 14:21:13 -0500

    PA3 startercode for those who want to start PA3 early - submission guide missing

---
+++ starter_code/PA3_Semantic_Analysis/ast/Node.java
@@ -0,0 +1,44 @@
+package ast;
+
+import mocha.Token;
+
+public abstract class Node implements Visitable {
+
+    private int lineNum;
+    private int charPos;
+
+    protected Node (int lineNum, int charPos) {
+        this.lineNum = lineNum;
+        this.charPos = charPos;
+    }
+
+    public int lineNumber () {
+        return lineNum;
+    }
+
+    public int charPosition () {
+        return charPos;
+    }
+
+    public String getClassInfo () {
+        return this.getClass().getSimpleName();
+    }
+
+    @Override
+    public String toString () {
+        return this.getClass().getSimpleName();
+    }
+
+    // Some factory methods for convenience
+    public static Statement newAssignment (int lineNum, int charPos, Expression dest, Token assignOp, Expression src) {
+        throw new RuntimeException("implement newAssignment factory method");
+    }
+
+    public static Expression newExpression (Expression leftSide, Token op, Expression rightSide) {
+        throw new RuntimeException("implement newExpression factory method");
+    }
+
+    public static Expression newLiteral (Token tok) {
+       throw new RuntimeException("implement newLiteral factory method");     
+    }
+}
---
+++ starter_code/PA3_Semantic_Analysis/mocha/Symbol.java
@@ -0,0 +1,17 @@
+package mocha;
+
+public class Symbol {
+
+    private String name;
+
+    // TODO: Add other parameters like type
+
+    public Symbol (String name) {
+        this.name = name;
+    }
+    public String name () {
+        return name;
+    }
+
+
+}
